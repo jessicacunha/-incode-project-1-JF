10d7cc062e1f43c891ad1f6b46233bf8c6ef13d1
Jéssica Suelen <jessiksu@hotmail.com>
2021-10-11 17:55:14 +0100

    Add Modal and form.js

--- www/form.js
+++ www/form.js
@@ -1,20 +1,27 @@
+var firstName = document.getElementById('InputFirstName')
+var surname = document.getElementById('InputSurname')
+var phone = document.getElementById('InputMobile')
+var email = document.getElementById('InputEmail')
+var message = document.getElementById('InputMessage')
+var modal = document.getElementById("modal");
+var sendButton = document.getElementById("send-btn");
+var closeButton = document.getElementById("close-btn");
 
-function FormFunction(){
+sendButton.onclick = function(event) { 
+    event.preventDefault()
+    typeof firstName.value == 'string' ? console.log("Name: " + firstName.value) : NaN
+    typeof surname.value == 'string' ? console.log("Surname: " + surname.value) : NaN
+    email.value.includes('@') == true ? console.log("E-mail: " + email.value) : NaN
+    typeof phone.value == 'number' ? console.log("Phone: " + phone.value) : NaN
+    typeof message.value == 'string' ? console.log("Message: " + message.value) : NaN
+    modal.style.display = "flex";
+}
 
-    var name = document.getElementById("InputFirstName").value;
-    var surname = document.getElementById("InputSurname").value;
-    var phone = document.getElementById("InputMobile").value;
-    var checkEmail = document.getElementById("InputEmail").value;
-    var message = document.getElementById("InputMessage").value;
-    var error_message = document.getElementById("error_message");
-    error_message.style.padding = "10px";
-
-
-
-    console.log("Name: " + name);
-    console.log("Surname: " + surname)
-    console.log("E-mail: " + checkEmail);
-    console.log("Phone: " + phone);
-    console.log("Message: " + message);
-    return true;
+closeButton.onclick = function() { 
+    modal.style.display = "none";
+}
+window.onclick = function(event) { 
+if (event.target == modal) {
+    modal.style.display = "none";
+}
 }
